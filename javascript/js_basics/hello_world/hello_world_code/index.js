101885d8defb5cd692046b51be0d138223f7624b
Logan Moecher <lmoecher@gmail.com>
2024-06-19 19:40:03 -0400

    hello world page done

--- javascript/js_basics/hello_world/hello_world_code/index.js
+++ javascript/js_basics/hello_world/hello_world_code/index.js
@@ -1,3 +1,4 @@
-// Why's and How's so develops know what is going on
+// Comment lines are great for Why's and How's 
+// This is so other developers know what is going on
 // First Javascript basic code for every beginner
     console.log('Hello World');
